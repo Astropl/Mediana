751cc92ad074ec5dd60e8fa42d5f0b5f926c0c06
Pawel <martys.pawel@gmail.com>
2018-11-07 18:45:37 +0100

    second ver

---
+++ src/main/java/astro/pl/App.java
@@ -0,0 +1,26 @@
+package astro.pl;
+
+import java.util.Arrays;
+
+public class App {
+    static double aver1, aver2, mediana;
+
+    public static void main(String[] args) {
+        double mediana = mediana(new double[]{1.0, 4.0, 16.0, 2.0, 23.0, 3.0, 27.0, 87.0, 4.0, 9.0, 77.0, 8.0});
+        System.out.println("Mediana to :" + mediana);
+    }
+
+    static double mediana(double[] tab) {
+        int amountOfElements = tab.length;
+        Arrays.sort(tab);
+        if (amountOfElements % 2 == 0) {
+            aver1 = (tab[tab.length / 2]);//mediana jest parzysta
+            aver2 = (tab[(tab.length / 2) - 1]);
+            mediana = (aver1 + aver2) / 2;
+        } else {
+            mediana = (tab[tab.length / 2]);//mediana jest nie parzysta
+        }
+        return mediana;
+    }
+}
+
